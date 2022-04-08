1957a78c926aa188dc233ae541a739ba23b3014a
piedeboer96 <piedeboer96@gmail.com>
2022-04-08 16:50:55 +0200

    start graph pie, tom

---
+++ core/src/com/project_1_2/group_16/ai/NavigationGraph.java
@@ -0,0 +1,17 @@
+package com.project_1_2.group_16.ai;
+
+import com.project_1_2.group_16.App;
+
+public class NavigationGraph {
+
+    //Empty matrix for flood-fill
+    int size_X, size_Y = (int)App.FIELD_DETAIL;
+    int[][] matrixParcour = new int[size_X][size_Y];
+
+    //
+
+
+
+
+
+}
