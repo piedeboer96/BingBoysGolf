4907778406c51a15033fd971358fe5c940e740f5
ThomasVroom <98174530+R2Xdj@users.noreply.github.com>
2022-04-11 21:19:29 +0200

    inputscreen

---
+++ core/src/com/project_1_2/group_16/titlescreen/TitleScreen.java
@@ -0,0 +1,101 @@
+package com.project_1_2.group_16.titlescreen;
+
+import javax.swing.*;
+
+import com.project_1_2.group_16.App;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TitleScreen {
+
+    JFrame frame;
+    JLabel picLabel;
+    JButton buttonY;
+    JButton buttonQ;
+    JLabel labelA;
+    JLayeredPane layeredPane;
+    public InputScreen inputScreen;
+
+    static int width = 960;
+    static int height = 640;
+
+    /**
+     * Set up the main menu's GUI
+     */
+    public void setUpGUI() {
+
+        //defining fonts
+        Font large = new Font("Serif",Font.BOLD,26);
+        Font medium = new Font("Serif",Font.PLAIN,15);
+
+        Color backGroundColor = new Color(59,87,72,255);
+        //Color fontColor = new Color(255,255,255,255);
+
+        picLabel = new JLabel(new ImageIcon(App.os_is_windows ? "./assets/golfTheme.png" : "../assets/golfTheme.png"));
+        picLabel.setBounds(0, 0, width, height);
+
+        buttonY = new JButton("Start");
+        buttonY.setFont(medium);
+        buttonY.setBackground(Color.decode("#676d88"));
+        buttonY.setForeground(Color.decode("#03d8f6"));
+        buttonY.setBounds(100, 270, 80, 30);
+
+        buttonQ = new JButton("Quit");
+        buttonQ.setFont(medium);
+        buttonQ.setBackground(Color.decode("#676d88"));
+        buttonQ.setForeground(Color.decode("#03d8f6"));
+        buttonQ.setBounds(210, 270, 90, 30);
+        buttonQ.setFocusable(false);
+
+        labelA = new JLabel("Welcome");
+        labelA.setFont(large);
+        labelA.setBackground(Color.decode("#676d88"));
+        labelA.setForeground(Color.decode("#03d8f6"));
+        labelA.setBounds(100, 100, 240, 40);
+
+        layeredPane = new JLayeredPane();
+        layeredPane.setBackground(backGroundColor);
+        layeredPane.setBounds(0, 0, width, height);
+        layeredPane.add(picLabel, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(buttonY, JLayeredPane.DRAG_LAYER);
+        layeredPane.add(buttonQ, JLayeredPane.DRAG_LAYER);
+        layeredPane.add(labelA, JLayeredPane.DRAG_LAYER);
+
+        frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setTitle("The G-Boys");
+        frame.setResizable(false);
+        frame.add(layeredPane);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void setUpButtonListeners() {
+        ActionListener buttonListener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                Object checkButton = ae.getSource();
+
+                if (checkButton == buttonY) {
+                    inputScreen = new InputScreen();
+                    inputScreen.setUpGUI();
+                    frame.dispose();
+                }
+
+                else if (checkButton == buttonQ )
+                {
+                   frame.dispose();
+                }
+            }
+        };
+
+        buttonY.addActionListener(buttonListener);
+        buttonY.setFocusable(false);
+        buttonQ.addActionListener(buttonListener);
+        buttonQ.setFocusable(false);
+    }
+}
--- desktop/src/com/project_1_2/group_16/DesktopLauncher.java
+++ desktop/src/com/project_1_2/group_16/DesktopLauncher.java
@@ -2,61 +2,49 @@ package com.project_1_2.group_16;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.math.Vector2;
+import com.project_1_2.group_16.titlescreen.TitleScreen;
 
-public class DesktopLauncher {
+public class DesktopLauncher extends Thread {
+
+	public final static DesktopLauncher launch = new DesktopLauncher();
 
 	public static void main (String[] arg) {
-
-		// set it false if you don't want to see the title and the input screen
-		boolean runTitleScreen = true;
-
 		// configure os
 		if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
 			App.os_is_windows = true;
 		}
 
-		//Uncomment for floadFIll
-//		long start = System.currentTimeMillis();
-//		NavigationGraph.fillGraphTable();
-//		NavigationGraph.fload_fill((int)NavigationGraph.flood_i,(int)NavigationGraph.flood_j);
-//		long end = System.currentTimeMillis();
-//		System.out.println("runtime: " + (end-start));
-		//System.out.println(Arrays.deepToString(matrixParcour));
+		// set it false if you don't want to see the title and the input screen
+		boolean runTitleScreen = false;
+
+		/*Uncomment for floadFIll
+		long start = System.currentTimeMillis();
+		NavigationGraph.fillGraphTable();
+		NavigationGraph.fload_fill((int)NavigationGraph.flood_i,(int)NavigationGraph.flood_j);
+		long end = System.currentTimeMillis();
+		System.out.println("runtime: " + (end-start));
+		//System.out.println(Arrays.deepToString(matrixParcour));//*/
 
 		if (runTitleScreen) {
 			TitleScreen titleScreen = new TitleScreen();
 			titleScreen.setUpGUI();
 			titleScreen.setUpButtonListeners();
+
+			while (titleScreen.inputScreen == null || !titleScreen.inputScreen.status) {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {e.printStackTrace();}
+			}
 		}
-		else {
-			// launch game
-			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-			config.setWindowedMode(Lwjgl3ApplicationConfiguration.getDisplayMode().width,
-					Lwjgl3ApplicationConfiguration.getDisplayMode().height);
-			config.setForegroundFPS(30);
-			config.useVsync(false);
-			new Lwjgl3Application(new App(), config);
-		}
+		// launch game
+		launch.start();
 	}
 
 	/**
-	 * this method will run the game, it is useful for running the game from other classes
+	 * Runs the application
 	 */
-	public static void run (){
-
-
-		// get inputs from the Input screen and set them for the App class
-		// (call to input screen)
-		 App.gV = new Vector2(InputScreen.x0, InputScreen.y0);
-		 App.tV = new Vector2(InputScreen.xt, InputScreen.yt);
-		 App.tR = InputScreen.r;
-		 App.NUMBER_OF_SANDPITS = InputScreen.sandPitsNum;
-		 App.NUMBER_OF_TREES = InputScreen.treesNum;
-		 App.userOrBot = InputScreen.userOrBot;
-		 System.out.println(App.userOrBot);
-
-		// launch game
+	@Override
+	public void run() {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setWindowedMode(Lwjgl3ApplicationConfiguration.getDisplayMode().width,
 				Lwjgl3ApplicationConfiguration.getDisplayMode().height);
