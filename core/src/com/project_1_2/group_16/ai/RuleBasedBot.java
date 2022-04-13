d9bc4bdff78412eeb4f9264c140e4f264cc58893
Akita67 <l.bijman@student.maastrichtuniversity.nl>
2022-04-13 20:18:12 +0200

    update : Implementation of the RuleBasedBot (not fully running at the moment)

--- core/src/com/project_1_2/group_16/ai/RuleBasedBot.java
+++ core/src/com/project_1_2/group_16/ai/RuleBasedBot.java
@@ -1,11 +1,43 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.project_1_2.group_16.ai;
 
-/**
- * NOTE TO OTHERS: Please write a description here of how the RuleBasedBot works.
- */
+import com.project_1_2.group_16.gamelogic.Game;
+import com.project_1_2.group_16.math.StateVector;
 
+import java.util.Random;
 
 public class RuleBasedBot {
+    static final int Population = 100;
+    static Random rand = new Random();
+    static final float max = 1.0F;
+    static int score;
+    static int bestScore;
+    static boolean scoreInitialise = false;
+    static float float_randomX;
+    static float float_randomY;
+    public static StateVector sv;
+    public static StateVector newsv;
 
+    public RuleBasedBot() {
+    }
 
+    public static void BestShot() {
+        for(int i = 0; i < Population; ++i) {
+            float_randomX = rand.nextFloat() * 1.0F;
+            float_randomY = rand.nextFloat() * 1.0F;
+            sv = new StateVector(0.0F, 0.0F, float_randomX, float_randomY);
+            //score = Game.runWithAI(sv);
+            if (!scoreInitialise || bestScore < score) {
+                scoreInitialise = true;
+                bestScore = score;
+                newsv = sv;
+                System.out.println(bestScore);
+            }
+        }
+
+    }
 }
