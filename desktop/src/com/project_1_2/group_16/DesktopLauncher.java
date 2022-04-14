02013d984843d4b9f4265c7ebd4a534db1cb2657
ThomasVroom <98174530+R2Xdj@users.noreply.github.com>
2022-04-14 16:35:31 +0200

    info about jar building

--- desktop/src/com/project_1_2/group_16/DesktopLauncher.java
+++ desktop/src/com/project_1_2/group_16/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.project_1_2.group_16;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.project_1_2.group_16.ai.FloodFill;
 import com.project_1_2.group_16.titlescreen.TitleScreen;
 
 public class DesktopLauncher {
@@ -12,7 +11,6 @@ public class DesktopLauncher {
 		if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
 			App.os_is_windows = true;
 		}
-
 
 		// create titlescreen
 		TitleScreen titleScreen = new TitleScreen();
@@ -33,5 +31,11 @@ public class DesktopLauncher {
 		config.setForegroundFPS(30);
 		config.useVsync(false);
 		new Lwjgl3Application(new App(), config);
+
+		// reminder for self to build executable jar:
+		// - open new terminal
+		// - cd to desktop
+		// - gradle dist command will create an executable jar in build/libs
+		// use launch4j to wrap the jar
 	}
 }
