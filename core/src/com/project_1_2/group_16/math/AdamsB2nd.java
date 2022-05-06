395efc6126c8e3533c75dbce6270c025afc248b6
Rafali Azzam Arfan <rafali.azzam2245@gmail.com>
2022-05-06 13:26:58 +0200

    Added AdamsM4

--- core/src/com/project_1_2/group_16/math/AdamsB2nd.java
+++ core/src/com/project_1_2/group_16/math/AdamsB2nd.java
@@ -17,31 +17,32 @@ public class AdamsB2nd implements NumericalSolver{
                 sv1 = new StateVector(sv.x, sv.y, sv.vx, sv.vy);
                 this.partialDerivatives = Terrain.getSlope(new float[] {sv.x, sv.y}, h);
                 count++;
-                return;
             }else if (count==1){
                 rk2Bootstrap.solve(h, sv);
                 sv2 = new StateVector(sv.x, sv.y, sv.vx, sv.vy);
                 this.partialDerivatives = Terrain.getSlope(new float[] {sv.x, sv.y}, h);
                 count++;
-                return;
             }
+        }else {
+            Derivation deriv1 = Derivation.getDerivation(sv1, h, new Derivation(), 0);
+            Derivation deriv2 = Derivation.getDerivation(sv2, h, new Derivation(), 0);
+
+            float pos_x1, pos_y1, vel_x1, vel_y1;
+            pos_x1 = sv.x + (h / 2f) * (3 * deriv2.dx_dt - deriv1.dx_dt);
+            pos_y1 = sv.y + (h / 2f) * (3 * deriv2.dy_dt - deriv1.dy_dt);
+            vel_x1 = sv.vx + (h / 2f) * (3 * deriv2.dvx_dt - deriv1.dvx_dt);
+            vel_y1 = sv.vy + (h / 2f) * (3 * deriv2.dvy_dt - deriv1.dvy_dt);
+
+            sv.x = pos_x1;
+            sv.y = pos_y1;
+            sv.vx = vel_x1;
+            sv.vy = vel_y1;
+
+            sv2 = sv1;
+            sv1 = new StateVector(sv.x, sv.y, sv.vx, sv.vy);
+
+            this.partialDerivatives = Terrain.getSlope(new float[]{sv.x, sv.y}, h);
         }
-        Derivation deriv1 = Derivation.getDerivation(sv1, h, new Derivation(), 0);
-        Derivation deriv2 = Derivation.getDerivation(sv2, h, new Derivation(), 0);
-
-        float pos_x1, pos_y1, vel_x1, vel_y1;
-        pos_x1 = sv.x + (h/2f) * (3 * deriv2.dx_dt - deriv1.dx_dt);
-        pos_y1 = sv.y + (h/2f) * (3 * deriv2.dy_dt - deriv1.dy_dt);
-        vel_x1 = sv.vx + (h/2f) * (3 * deriv2.dvx_dt - deriv1.dvx_dt);
-        vel_y1 = sv.vy + (h/2f) * (3 * deriv2.dvy_dt - deriv1.dvy_dt);
-
-        sv.x = pos_x1; sv.y = pos_y1; sv.vx = vel_x1; sv.vy = vel_y1;
-
-        sv2 = sv1;
-        sv1 = new StateVector(sv.x, sv.y, sv.vx, sv.vy);
-
-        this.partialDerivatives = Terrain.getSlope(new float[] {sv.x, sv.y}, h);
-
         if (Physics.magnitude(sv.vx, sv.vy) < h) {
             float[] partialDerivatives = this.getPartialDerivatives();
             sv.stop = Physics.magnitude(partialDerivatives[0], partialDerivatives[1]) < Terrain.getStaticFriction(sv);
--- core/src/com/project_1_2/group_16/math/NumericalSolver.java
+++ core/src/com/project_1_2/group_16/math/NumericalSolver.java
@@ -9,6 +9,9 @@ public interface NumericalSolver {
 
     public static final int ADAMSB2 = 4;
 
+    public static final int ADAMSM4 = 5;
+
+
     public void solve(float h, StateVector sv);
 
     public float[] getPartialDerivatives();
