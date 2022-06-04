582b4ae7ed200b992b9ac0f1842cc6366acd08eb
ThomasVroom <98174530+R2Xdj@users.noreply.github.com>
2022-06-04 15:13:10 +0200

    working level decoder
    (default values moved to json file)

--- core/src/com/project_1_2/group_16/Input.java
+++ core/src/com/project_1_2/group_16/Input.java
@@ -1,92 +1,70 @@
 package com.project_1_2.group_16;
 
 import com.badlogic.gdx.math.Vector2;
-import com.project_1_2.group_16.themes.DefaultTheme;
 import com.project_1_2.group_16.themes.Theme;
 
 /**
- * Handles all user input for the game. 
- * Default values in here are shown as the default values on the GUI.
+ * Handles all user input for the game.
  */
 public class Input {
 
     /**
-     * The theme (textures) for the game.
-     */
-    public static Theme THEME = new DefaultTheme();
-
-    /**
      * Starting position of the golfball.
      */
-    public static Vector2 V0 = new Vector2(-3, 0);
+    public static Vector2 V0;
 
     /**
      * Position of the hole.
      */
-    public static Vector2 VT = new Vector2(4f, 1f);
+    public static Vector2 VT;
 
     /**
      * Radius of the hole.
      */
-    public static float R = 0.15f;
-
-    /**
-     * Kinetic Friction.
-     */
-    public static float MUK =  0.08f;
-
-    /**
-     * Static Friction.
-     */
-    public static float MUS = 0.2f;
-
-    /**
-     * Kinetic Friction (sandpit).
-     */
-    public static float MUKS = 4 * MUK;
-
-    /**
-     * Static Friction (sandpit).
-     */
-    public static float MUSS = 4 * MUS;
+    public static float R;
 
     /**
      * Height Function.
      */
-    public static String H = "0.4*(0.9 - Math.pow(Math.E, (-1*(x*x+y*y))/8))";
-    // examples (for testing):
-    // Math.pow(Math.E,(-(x*x + y*y))/40)-0.5
-    // 0.4*(0.9 - Math.pow(Math.E, (-1*(x*x+y*y))/8))
+    public static String H;
 
     /**
      * Number of trees.
      */
-    public static int TREES = 0;
+    public static int TREES;
 
     /**
      * Number of sandpits.
      */
-    public static int SAND = 0;
+    public static int SAND;
 
     /**
      * Pre-computed spline for bicubic interpolation.
      */
-    public static float[][] BICUBIC_INPUT = {
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}
-    };
+    public static float[][] BICUBIC_INPUT;
+
+    /**
+     * Kinetic Friction.
+     */
+    public static float MUK;
+
+    /**
+     * Static Friction.
+     */
+    public static float MUS;
+
+    /**
+     * Kinetic Friction (sandpit).
+     */
+    public static float MUKS;
+
+    /**
+     * Static Friction (sandpit).
+     */
+    public static float MUSS;
+
+    /**
+     * The theme (textures) for the game.
+     */
+    public static Theme THEME;
 }
--- core/src/com/project_1_2/group_16/misc/LevelDecoder.java
+++ core/src/com/project_1_2/group_16/misc/LevelDecoder.java
@@ -2,17 +2,60 @@ package com.project_1_2.group_16.misc;
 
 import java.io.File;
 
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.JsonValue.JsonIterator;
+import com.project_1_2.group_16.Input;
+import com.project_1_2.group_16.gamelogic.Spline;
+import com.project_1_2.group_16.themes.DefaultTheme;
+
 /**
  * Auxiliary class for decoding a pre-computed level.
  */
 public class LevelDecoder {
+
+    private static final JsonReader PARSER = new JsonReader();
     
     /**
-     * Decode a pre-computed level.
+     * Decode a pre-computed level, and parse it to the input file.
      * @param level file containing the level data
      */
     public static void decode(File level) {
-        // TODO
-        System.out.println("TODO: decode level here");
+        // get the full level
+        JsonValue fullInput = PARSER.parse(new FileHandle(level));
+        
+        // start position
+        JsonValue startPosition = fullInput.get("starting_position");
+        Input.V0 = new Vector2(startPosition.getFloat("x"), startPosition.getFloat("y"));
+
+        // flag position
+        JsonValue flagPosition = fullInput.get("flag_position");
+        Input.VT = new Vector2(flagPosition.getFloat("x"), flagPosition.getFloat("y"));
+        Input.R = flagPosition.getFloat("radius");
+
+        // terrain
+        JsonValue terrain = fullInput.get("terrain");
+        Input.H = terrain.getString("height_function");
+        Input.TREES = terrain.getInt("number_of_trees");
+        Input.SAND = terrain.getInt("number_of_sandpits");
+        JsonIterator bicubicIterator = terrain.get("bicubic_input").iterator();
+        Input.BICUBIC_INPUT = new float[Spline.SPLINE_SIZE][Spline.SPLINE_SIZE];
+        for (int i = Spline.SPLINE_SIZE - 1; i >= 0; i--) {
+            Input.BICUBIC_INPUT[i] = bicubicIterator.next().asFloatArray();
+        }
+        
+        // friction coefficients
+        Input.MUK = fullInput.getFloat("kinetic_friction");
+        Input.MUS = fullInput.getFloat("static_friction");
+        Input.MUKS = fullInput.getFloat("kinetic_friction_sand");
+        Input.MUSS = fullInput.getFloat("static_friction_sand");
+
+        // theme
+        String theme = fullInput.getString("theme").toLowerCase();
+        switch (theme) {
+            default: Input.THEME = new DefaultTheme();
+        }
     }
 }
