2b8c95232646d3ea3c3dff077e34b01fe09fa68c
Rafali Azzam Arfan <rafali.azzam2245@gmail.com>
2022-06-10 12:13:12 +0200

    MAZE BOT CHANGES (ADDED FUTURISTIC SHOTS)

--- core/src/com/project_1_2/group_16/bot/ai/MazeBot.java
+++ core/src/com/project_1_2/group_16/bot/ai/MazeBot.java
@@ -8,45 +8,98 @@ import com.project_1_2.group_16.gamelogic.Game;
 import com.project_1_2.group_16.math.StateVector;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 
 //MazeBot, basically brute force with better Heuristics/Fitness function
 public class MazeBot extends AdvancedBot {
+    float[] bestCandidate = new float[4];
     public MazeBot(float startX, float startY, Game game){
         //Always set useFloodFill to true as maze bot relies on that specific Heuristic
         super(startX, startY, game, true);
     }
-
     @Override
     public List<Float> runBot() {
-        ArrayList<float[]> candidates = (ArrayList<float[]>) BotHelper.availableVelocities2();
-//        ArrayList<float[]> candidates = (ArrayList<float[]>) BotHelper.availableVelocities(getStartX(), getStartY());
-        float bestFitness = Integer.MAX_VALUE;
-        float [] bestCandidate = new float[2];
-        if(BotHelper.getFloodFillFitness(getStartX(), getStartY()) == 0){
-            float diffX = Input.VT.x - getStartX();
-            float diffY = Input.VT.y - getStartY();
-            Vector2 v2 = new Vector2(diffX, diffY);
-            v2.nor();
-            List<Float> toReturn = new ArrayList<Float>();
-            toReturn.add(v2.x);
-            toReturn.add(v2.y);
-            return toReturn;
+        if(BotHelper.getFloodFillFitness(getStartX(), getStartY()) <= 1){
+            return makeSimpleShot();
         }
-        for (float[] candidate : candidates){
-            StateVector sv = new StateVector(getStartX(), getStartY(), candidate[0], candidate[1]);
+        return makeHeuristic_BasedShot();
+    }
+
+    /**
+     * If the Heuristic Score is too low, no need to make a Heuristic-Based shot so just shoot into the direction
+     * of the Target
+     * @return list containing velocity w.r.t x and y direction
+     */
+    private List<Float> makeSimpleShot(){
+        float diffX = Input.VT.x - getStartX();
+        float diffY = Input.VT.y - getStartY();
+        Vector2 v2 = new Vector2(diffX, diffY);
+        v2.nor();
+        List<Float> toReturn = new ArrayList<Float>();
+        toReturn.add(v2.x);
+        toReturn.add(v2.y);
+        return toReturn;
+    }
+
+    /**
+     * Method to determine best shot based on the Heuristic-Score in a brute-force like manner
+     * we check two iterations into the future to optimize it
+     * @return list containing velocity w.r.t x and y direction
+     */
+    private List<Float> makeHeuristic_BasedShot(){
+        ArrayList<float[]> candidates = (ArrayList<float[]>) BotHelper.availableVelocities2();
+        ArrayList<Shot> shots = new ArrayList<Shot>();
+        float bestFitness = Integer.MAX_VALUE;
+        bestCandidate = new float[2];
+        for(float[] candidate_a : candidates){
+            StateVector sv = new StateVector(getStartX(), getStartY(), candidate_a[0], candidate_a[1]);
             getGame().runEngine(sv, null);
-            float [] endPosition = new float[] {sv.x, sv.y};
-            float curFitness =  BotHelper.getFloodFillFitness(endPosition[0], endPosition[1]);
-            System.out.println("fitness = " + curFitness);
-            if(curFitness < bestFitness){
-                bestFitness = curFitness;
-                bestCandidate = candidate;
+            float tempFitness = BotHelper.getFloodFillFitness(sv.x, sv.y);
+            shots.add(new Shot(sv.x, sv.y, candidate_a[0], candidate_a[1], tempFitness));
+        }
+        Shot bestShot = new Shot(0,0,0,0,0);
+        Collections.sort(shots);
+        for(int i=0; i<5; i++){
+            Shot firstShot = shots.get(i);
+            float tempBestFitness = Integer.MAX_VALUE;
+            float newPosX = firstShot.newX; float newPosY = firstShot.newY;
+            for(float[] candidate_b : candidates){
+                StateVector sv = new StateVector(newPosX, newPosY, candidate_b[0], candidate_b[1]);
+                getGame().runEngine(sv, null);
+                float tempFitness = BotHelper.getFloodFillFitness(sv.x, sv.y);
+                if(tempFitness < tempBestFitness){
+                    tempBestFitness = tempFitness;
+                }
+            }
+            if(tempBestFitness < bestFitness){
+                bestFitness = tempBestFitness;
+                bestShot = firstShot;
             }
         }
-        System.out.println("best Fitness = " + bestFitness);
         List<Float> toReturn = new ArrayList<Float>();
-        toReturn.add(bestCandidate[0]); toReturn.add(bestCandidate[1]);
+        toReturn.add(bestShot.velX); toReturn.add(bestShot.velY);
         return toReturn;
+    }
+
+    class Shot implements Comparable<Shot> {
+        float velX;
+        float velY;
+        float newX;
+        float newY;
+        float fitness;
+        public Shot(float newX, float newY, float velX, float velY, float fitness){
+            this.newX = newX;
+            this.newY = newY;
+            this.velX = velX;
+            this.velY = velY;
+            this.fitness = fitness;
+        }
+        //Sorts in Ascending Order based on Fitness (FloodFill distance)
+        @Override
+        public int compareTo(Shot other) {
+            return (int) (this.fitness - other.fitness);
+        }
     }
 }
